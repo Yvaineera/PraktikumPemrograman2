58c13e4487a67be789333bbbd29f2c05cb76cd72
Yvaineera <114866127+Yvaineera@users.noreply.github.com>
2023-10-17 00:10:30 +0800

    Update Soal3Main.java

--- praktikum3modul2/PRAK203_2210817220022_RayfhaZafiraAzzahra/Soal3Main.java
+++ praktikum3modul2/PRAK203_2210817220022_RayfhaZafiraAzzahra/Soal3Main.java
@@ -2,7 +2,7 @@ package PRAK203_2210817220022_RayfhaZafiraAzzahra;
 
 public class Soal3Main {
     public static void main(String[] args) {
-        Pegawai p1 = new Pegawai();  // Perbaikan: Menggunakan kelas Pegawai yang sudah diimpor
+        Pegawai p1 = new Pegawai(); 
         p1.nama = "Roi";
         p1.asal = "Kingdom of Orvel";
         p1.setJabatan("Assasin");
@@ -11,6 +11,6 @@ public class Soal3Main {
         System.out.println("Nama Pegawai: " + p1.getNama());
         System.out.println("Asal: " + p1.getAsal());
         System.out.println("Jabatan: " + p1.jabatan);
-        System.out.println("Umur: " + p1.umur + " Tahun");  // Perbaikan: Memperbaiki pengambilan umur
+        System.out.println("Umur: " + p1.umur + " Tahun");  
     }
 }
