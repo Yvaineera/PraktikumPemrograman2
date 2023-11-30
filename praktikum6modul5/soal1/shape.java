6890c436ff3b47b7c22bb74d10636c281b3e73f5
Yvaineera <114866127+Yvaineera@users.noreply.github.com>
2023-11-30 20:50:55 +0800

    Update shape.java

--- praktikum6modul5/soal1/shape.java
+++ praktikum6modul5/soal1/shape.java
@@ -3,9 +3,13 @@ package soal1;
 public abstract class shape {
     protected String shapeName;
 
-    public shape (String name){shapeName = name;}
+    public shape (String name){
+        shapeName = name;
+    }
 
     protected abstract Double area();
 
-    public String toString(){return shapeName;}
+    public String toString(){
+        return shapeName;
+    }
 }
