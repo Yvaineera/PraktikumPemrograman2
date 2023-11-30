6cbfddac41dac96f5508b1412d7e37c53949c544
Yvaineera <114866127+Yvaineera@users.noreply.github.com>
2023-11-30 20:51:24 +0800

    Update paint.java

--- praktikum6modul5/soal1/paint.java
+++ praktikum6modul5/soal1/paint.java
@@ -3,7 +3,9 @@ package soal1;
 public class paint {
     private double coverage;
 
-    public paint(double c){coverage = c;}
+    public paint(double c){
+        coverage = c;
+    }
 
     public double amount(shape s){
         System.out.println("Computing amount for "+ s);
